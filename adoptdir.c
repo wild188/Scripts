7d734cc5231f6cf6c8fae947a04f81df3ff41287
Billy <wild188@gmail.com>
2016-10-03 13:56:06 -0400

    copies files from directroy to current

--- adoptdir.c
+++ adoptdir.c
@@ -2,14 +2,87 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /*
  * This script should move all the contents of a subfolder to the current
  * directory and delete the subfolder.
  */
 
+void copyFile(char *source, char *dest){
+    int childExitStatus;
+    pid_t pid;
+    //int status = 0;
+    if (!source || !dest) {
+        /* handle as you wish */
+        exit(1);
+    }
+
+    pid = fork();
+
+    if (pid == 0) { /* child */
+        execl("/bin/cp", "/bin/cp", source, dest, (char *)0);
+    }
+    else if (pid < 0) {
+        /* error - couldn't start process - you decide how to handle */
+    }
+    else {
+        /* parent - wait for child - this has all error handling, you
+         * could just call wait() as long as you are only expecting to
+         * have one child process at a time.
+         */
+        pid_t ws = waitpid( pid, &childExitStatus, WNOHANG);
+        if (ws == -1)
+        { /* error - handle as you wish */
+        }
+
+        if( WIFEXITED(childExitStatus)) /* exit code in childExitStatus */
+        {
+            int status = WEXITSTATUS(childExitStatus); /* zero is normal exit */
+            /* handle non-zero as you wish */
+            if(status){
+              printf("Exited with error number %i\n", status);
+            }
+        }
+        else if (WIFSIGNALED(childExitStatus)) /* killed */
+        {
+        }
+        else if (WIFSTOPPED(childExitStatus)) /* stopped */
+        {
+        }
+    }
+}
+
 int copydir(char * curPath, char * subdir){
   //TODO
+  printf("Copying directory at %s\n", subdir);
+  DIR* dir;
+  struct dirent *ent;
+  if ((dir = opendir (subdir)) != NULL) {
+    /* print all the files and directories within directory */
+    while ((ent = readdir (dir)) != NULL) {
+      printf ("%s ", ent->d_name);
+      if(!(strcmp(ent->d_name, ".")) || !strcmp(ent->d_name, "..")){
+        printf("skipping\n");
+      }else{
+        printf("\n");
+        char * file = malloc((strlen(subdir) + strlen(ent->d_name)) * sizeof(char));
+        strcpy(file, subdir);
+        strcat(file, ent->d_name);
+        copyFile(file, curPath);
+      }
+    }
+    
+    printf("Trying to close direcory %s at %p\n", subdir, dir);
+    closedir(dir);
+  } else {
+    /* could not open directory */
+    perror ("Couldnt open dir \n");
+    return EXIT_FAILURE;
+  }
+  
   return 1;
 }
 
@@ -20,8 +93,7 @@ void cleardir(char * dir){
 //main method to capture arguments
 int main(int argc, char** argv){
   char * curPath;
-  char * subdir = "test";
-
+  char * subdir;
 
   printf("Hello world!\n");
   char temp[1024];
@@ -32,8 +104,24 @@ int main(int argc, char** argv){
   }else{
     printf("Path error.\n");
   }
+
+  if(argc > 0){
+    subdir = malloc((strlen(curPath) + strlen(argv[1]) + 1) * sizeof(char));
+    strcpy(subdir, curPath);
+    strcat(subdir, "/");
+    strcat(subdir, argv[1]);
+    printf("added /%s to current path resulting in %s\n", argv[1], subdir);
+  }
+
+  if(!subdir){
+    return 1;
+  }
   int result = copydir(curPath, subdir);
+
   if(result){
     cleardir(strcat(curPath, subdir));
   }
+
+  free(subdir);
+  free(curPath);
 }
