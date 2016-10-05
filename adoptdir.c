e8d650e7e5d043f28bc1ebfe7e5e840424d5fd63
Billy <wild188@gmail.com>
2016-10-05 01:05:39 -0400

    inline cp functions allows us not to use execl

--- adoptdir.c
+++ adoptdir.c
@@ -14,6 +14,70 @@
 char** copied;
 int numFiles;
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int mycp(const char *to, const char *from)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+    fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+
+        /* Success! */
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
+
+
 void copyFile(char *source, char *dest){
      int childExitStatus;
     pid_t pid;
@@ -26,9 +90,10 @@ void copyFile(char *source, char *dest){
     pid = fork();
 
     if (pid == 0) { /* child */
-        execl("/bin/cp", "/bin/cp", source, dest, (char *)0);
-        _exit(EXIT_FAILURE);
-        //exit(0);
+        //execl("/bin/cp", "/bin/cp", source, dest, (char *)0);
+        //_exit(EXIT_FAILURE);
+        mycp(dest, source);
+        exit(0);
     }
     else if (pid < 0) {
         /* error - couldn't start process - you decide how to handle */
@@ -107,7 +172,7 @@ int copydir(char * curPath, char * subdir){
 void cleardir(char * dir){
   //TODO
 
-  sleep(2);
+  //sleep(2);
 
   int i;
   for(i = 0; i < numFiles; i++){
